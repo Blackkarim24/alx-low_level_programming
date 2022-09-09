823764c868c84dfba531ca04afb84e061c385316
Uzoma Victor <victorvidic247@gmail.com>
2022-09-09 05:19:09 -0700

    task 8 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
 	return (1);
 }

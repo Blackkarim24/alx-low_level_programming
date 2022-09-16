028fe5f1fc76aec1aa76b0ef302b34857c9b6259
Uzoma Victor <victorvidic247@gmail.com>
2022-09-15 19:33:46 -0700

    commit for task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
+ *main - causes an infinite loop
+ *Return: 0
  */
 
 int main(void)
@@ -14,9 +14,9 @@ int main(void)
 
 	/**
 	 * while(i < 10)
-	 * {
-	 *     putchar(i);
-	 * }
+	 *{
+	 *	putchar(i);
+	 *}
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-Copy this main file. Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop
+This is the README.md file for all the tasks in this project

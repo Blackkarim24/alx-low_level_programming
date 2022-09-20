4eb68e5ed02cfa169948dfae36a739c99da53a02
Uzoma Victor <victorvidic247@gmail.com>
2022-09-20 10:07:32 -0700

    main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char);
+void reset_to_98(int *);
+void swap_int(int *, int *);
+int _strlen(char *);
+void _puts(char *);
+void print_rev(char *);
+void rev_string(char *);
+void puts2(char *);
+void puts_half(char *);
+void print_array(int *, int);
+char *_strcpy(char *, char *);
+int _atoi(char *);
+
+#endif

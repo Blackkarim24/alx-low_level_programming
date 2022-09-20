697f61d739c987e036ad5ba2fd1b6da946cd94a4
Uzoma Victor <victorvidic247@gmail.com>
2022-09-20 08:20:00 -0700

    main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*@c: prints character
+*_putchar: instead of printf
+*Return: Success
+*/
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif

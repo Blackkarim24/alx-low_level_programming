1428794d524e8414455771e7b8ffb2bee4bb4397
Uzoma Victor <victorvidic247@gmail.com>
2022-09-21 13:58:07 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_rev - function that prints a string in reverse
+* followed by a new line to stdout
+* @s: string to print
+*/
+
+void print_rev(char *s)
+{
+	int i, j;
+	char c;
+
+	i = 0;
+	while (*(s + i))
+	{
+		i++;
+	}
+	c = *(s + i + 1);
+	j = 1;
+	while (c)
+	{
+		_putchar(c);
+		j++;
+		c = *(s + i - j);
+	}
+	_putchar('\n');
+}

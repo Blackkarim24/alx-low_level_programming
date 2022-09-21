c6c561c120faa8505383b49f73925704479d398a
Uzoma Victor <victorvidic247@gmail.com>
2022-09-21 15:59:25 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+* rev - function that reverse swap two char
+* @a: first parameter
+* @b: second parameter
+* Return: void
+*/
+
+void rev(char *a, char *b)
+{
+	char tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+* rev_string - function that reverses a string
+* @s: string to reverse
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (*(s + i))
+	{
+		i++;
+	}
+	i--;
+	j = 0;
+	while (i > j)
+	{
+		rev(s + i, s + j);
+		j++;
+		i--;
+	}
+}
